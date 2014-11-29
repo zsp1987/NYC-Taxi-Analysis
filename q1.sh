49518595e7aaabbae5568bad16b566433d58b482
Guang XIONG <gx239@nyu.edu>
2014-11-29 11:45:38 -0500

    modified q1.sh a bit

--- q1.sh
+++ q1.sh
@@ -2,7 +2,7 @@
 rm -rf output/q1
 $HADOOP_HOME/bin/hadoop  jar $HADOOP_HOME/contrib/streaming/hadoop-streaming-1.0.3.jar \
 	-D mapred.reduce.tasks=2 \
-    -input trip_date_test \n
+    -input inputtest/trip_date_test \
     -output output/q1 \
     -mapper q1map.py \
-    -reducer q2rd.py \
+    -reducer q1rd.py \
