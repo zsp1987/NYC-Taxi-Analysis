5af4360a80f9c23ed10945b48d10a3fea29cb1d3
haochen <hc1351@nyu.edu>
2014-12-01 18:17:29 -0500

    every day revenue

--- mytest.py
+++ mytest.py
@@ -1,11 +1,18 @@
 __author__ = 'haochen'
 
-f = open('BigEvent','r')
-line = f.readline().strip().split()
-print line
-EventMonth = line[1]
-print EventMonth
-EventDay = line[1].strip().split('-')
-print EventDay
-if '7' in EventDay:
-    print True;
+
+f1 = open('BigEvent','r')
+Event = f1.readline().strip().split()
+EventMonth = Event[0]
+EventDay = Event[1].strip().split('-')
+f = open('output/EveryDayRevenue/part-00000','r')
+line = f.readlines()
+for day in line:
+    day1 = day.strip().split('\t')
+    day2 = day1[0].strip().split('-')
+    month = day2[1]
+    dayRevenue = day2[2]
+    if EventMonth == month:
+        if dayRevenue in EventDay:
+            print day
+
