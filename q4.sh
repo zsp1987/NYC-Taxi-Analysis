727451a18a38720e201faed6cd45485322dc11dd
haochen <hc1351@nyu.edu>
2014-12-08 14:51:24 -0500

    every day revenue

--- q4.sh
+++ q4.sh
@@ -1,6 +1,6 @@
 rm -rf output/q4
 $HADOOP_HOME/bin/hadoop  jar $HADOOP_HOME/contrib/streaming/hadoop-streaming-1.0.3.jar \
-    -input inputtest/trip_fare_test* \
+    -input inputtest/trip_fare_1.csv \
     -output output/q4/ \
     -mapper q4map.py \
     -reducer q4rd.py
